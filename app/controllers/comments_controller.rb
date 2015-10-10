518ca3f9deaa6cfb7fde9698228846ce63db39b0
edwin0258 <edwin0258@gmail.com>
2015-10-10 15:38:38 -0700

    Get form when new button is pressed

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,18 +1,22 @@
 class CommentsController < ApplicationController
-	before_action :all_comments, only: [:index, :create]
-	respond_to :html, :js
-	def new
-		@comment = Comment.new
-	end
-	def create
-		@comment = Comment.create(comment_params)
-	end
+  before_action :all_comments, only: [:index, :create]
+  respond_to :html, :js
 
-	private
-		def all_comments
-			@comment = Comment.all
-		end
-		def comment_params
-			params.require(:comment).permit(:content)
-		end
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = Comment.create(comment_params)
+  end
+
+  private
+
+    def all_comments
+      @comments = Comment.all
+    end
+
+    def comment_params
+      params.require(:comment).permit(:content)
+    end
 end
