e094a43695b28bbfab5068dd8621d15007eaa57d
edwin0258 <edwin0258@gmail.com>
2015-10-10 14:26:14 -0700

    Setup controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,18 @@
 class CommentsController < ApplicationController
-	def index
-		@comments = Comment.all
+	before_action :all_comments, only: [:index, :create]
+	respond_to :html, :js
+	def new
+		@comment = Comment.new
 	end
+	def create
+		@comment = Comment.create(comment_params)
+	end
+
+	private
+		def all_comments
+			@comment = Comment.all
+		end
+		def comment_params
+			params.require(:comment).permit(:content)
+		end
 end
